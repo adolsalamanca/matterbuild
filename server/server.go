96e5ec188b594d53389c746e6f9f26155b21f998
Adolfo Rodriguez <adolsalamanca@gmail.com>
2020-07-27 20:58:12 +0200

    Apply suggested changes

--- server/server.go
+++ server/server.go
@@ -502,6 +502,7 @@ git commit plugins.json data/statik/statik.go -m "Add %[1]s of %[2]s to the Mark
 git push --set-upstream origin %[3]s
 git checkout master
 `, tag, repo, branch)
+<<<<<<< HEAD
 
 		url := fmt.Sprintf(
 			"https://github.com/mattermost/mattermost-marketplace/compare/production...%s?quick_pull=1&labels=3:+QA+Review,2:+Dev+Review",
@@ -511,6 +512,10 @@ git checkout master
 			"@%s A Plugin was successfully signed and uploaded to Github and S3.\nTag: **%s**\nRepo: **%s**\n[Release Link](%s)\nTo add this release to the Plugin Marketplace run inside your local Marketplace repository:\n```%s\n```\nUse %s to open a Pull Request.",
 			username, tag, repo, releaseURL, marketplaceCommand, url,
 		)
+=======
+		url := fmt.Sprintf("https://github.com/mattermost/mattermost-marketplace/compare/production...%s?quick_pull=1&labels=3:+QA+Review,2:+Dev+Review", branch)
+		msg = fmt.Sprintf("Plugin was successfully signed and uploaded to Github and S3.\nTag: **%s**\nRepo: **%s**\nCommitSHA: **%s**\n[Release Link](%s)\nTo add this release to the Plugin Marketplace run inside your local Marketplace repository:\n```%s\n```\nUse %s to open a Pull Request.", tag, repo, commitSHA, releaseURL, marketplaceCommand, url)
+>>>>>>> 8384631... Apply suggested changes
 		color := "#0060aa"
 		if err := PostExtraMessages(slashCommand.ResponseUrl, GenerateEnrichedSlashResponse("Pluging Release Process", msg, color, IN_CHANNEL)); err != nil {
 			LogError("failed to post success msg through PostExtraMessages err=%s", err.Error())
